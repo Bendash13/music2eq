924f20ae3c5d41f7a35cf43d664fdf834845575d
Ben Hartshorn <bendash13@gmail.com>
2020-11-25 10:56:59 +1300

    1

--- music2eq.py
+++ music2eq.py
@@ -1,4 +1,16 @@
 # convert MIDI to raw data
+import py_midicsv as pm
+
+
+def parse_csv():
+    csv_string = pm.midi_to_csv("example.midi")
+    print(csv_string)
+
+
 # split data into two groups
+def splitter():
+    
 # timing fix?
 # scale velocity to correct scaling for converter
+
+
