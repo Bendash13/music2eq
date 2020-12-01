c350de56811181f14b9019353beae7ca85793a9a
Bendash13 <Bendash13@gmail.com>
2020-12-01 23:14:49 +0000

    convert tempo to seconds and impletment Dani's pitch splitting function

--- music2eq.py
+++ music2eq.py
@@ -1,18 +1,18 @@
 import py_midicsv as pm
 import pandas as pd
-from csv import reader
 import re
 
 
 miditestfile = 'cherry-ripe-piano-solo.mid'
 pattern = re.compile(r'2, (\d+), Note_on_c, \d+, (\d+), (\d+)')
+tempo_pattern = re.compile(r'1, 0, Tempo, (\d+)')
 
 # convert MIDI to raw data
 def convert_midi2list(filename):
     csv_string = pm.midi_to_csv(filename)
     return csv_string
 
-# pulls out the csv file - potentially take out
+
 def convert_list2listolists(csv_string):
     listolists = []
     for bit in csv_string:
@@ -22,20 +22,21 @@ def convert_list2listolists(csv_string):
 #    df = pd.DataFrame([x.split(',') for x in csv_string.split('/n')])
 #    
 #    df.to_csv('./midi.csv', sep=',',index=False)
-return listolists
+    return listolists
+    
 
 
 def checker(string, list):
-    tcount = 0 # counts if the string is present
-    fcount = 0 # counts if the string is not present
+    tcount = 0
+    fcount = 0
     for item in list:
         if string in item:
             tcount+=1
         else:
             fcount+=1
-    print(f'T={tcount} F={fcount}') # prints no. present and not present
+    print(f'T={tcount} F={fcount}')
     
-# cuts out the time, pitch and velocity so we know what to work with    
+    
 def parse_list(list):
     note_list = []
     for line in list:
@@ -46,34 +47,51 @@ def parse_list(list):
 #            time = b[1]
 #            pitch = b[2]
 #            velocity = b[3]
-            result = [b[1], b[2], b[3]]
+            result = [int(b[1]), int(b[2]), int(b[3])]
             note_list.append(result)
-            pitch_list.append(b[2])
-    return note_list, pitch_list
+            
+    return note_list
 
-# function that finds the centre note to split the song
-def centre_pitch(list):
-    avg = sum(list)/len(list)
-    middle = round(avg)
-    return middle
+    
+def bpm_get(thing):
+    for line in thing:
+        b = tempo_pattern.match(line)
+        if b == None:
+            pass
+        else:
+            return 60000000/int(b[1])
 
-# split data into two groups - changed into x and y coordinates based on pitch
-# now splits at the identified centre note rather than through manual iteration process, may need further adjustment
-def splitter(list, middle):
+            
+def convert2sec(val, bpm):
+    bpval = (60/bpm)/2
+    cval = val/480
+    return bpval*cval
+    
+            
+# split data into two groups       
+        
+def splitter(list):
     x = []
     y = []
     for item in list:
-        if int(item[1]) >= middle:
+        if int(item[1]) >= 58:
             y.append(item)
         else:
             x.append(item)
     return x, y
         
-# finds number of x and y data to compare
+# scale velocity to correct scaling for converter
+
 def len_lists_of_lists(list):
     print(f'y = {len(list[1])} x = {len(list[0])}')
 
-# scale velocity to correct scaling for converter
+
+def centre_pitch(keylist):
+    avg = sum(keylist)/len(keylist)
+    middle = round(avg)
+    return middle
+    
+    
 def scale_velocity(list):
     new_list = []
     for i in list:
@@ -81,8 +99,8 @@ def scale_velocity(list):
         result = [int(i[0]), int(i[1]), val]
         new_list.append(result)
     return new_list
-            
-            
+
+
 def pdscale(val):
     return ((val/127)-0.5)*40
     
@@ -91,6 +109,7 @@ def convert2dataframe(listolists):
     df = pd.DataFrame.from_records(listolists, columns=['time', 'pitch', 'velocity'])    
 #    df = pd.DataFrame.from_records(listolists, columns=['section', 'time', 'command', 'channel', 'pitch', 'velocity', 'other'])
     return df
+    
     
 def thing():
     converted = convert_midi2list(miditestfile)
@@ -102,20 +121,21 @@ def thing():
 
 def thing2():
     converted = convert_midi2list(miditestfile)
+    bpm = bpm_get(converted)
     parsed_list = parse_list(converted)
     df = convert2dataframe(parsed_list)
-#    df = df.drop(df[df.velocity == 0].index)
     df = df[df.velocity != 0]
     df.velocity = df.velocity.apply(pdscale)
-    x = df[df.pitch >= 58]
-    y = df[df.pitch < 58]
+    df.time = df.time.apply(convert2sec, args=(bpm,))
+    split_pitch = centre_pitch(df.pitch.tolist())
+    x = df[df.pitch >= split_pitch]
+    y = df[df.pitch < split_pitch]
     x = x.groupby('time', as_index=False).mean()
     y = y.groupby('time', as_index=False).mean()
     statx = x.describe()
     staty = y.describe()
-    print(statx)
-    print(staty)
-   
+    print(statx, staty)
+    
     
 def integerer(x):
     return int(x)
@@ -131,4 +151,4 @@ def tester():
     
     
     
-    thing2()
+thing2()
